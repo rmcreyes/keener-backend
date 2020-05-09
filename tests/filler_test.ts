bc77c145b9b772fc0c3340da70c971c6e4c247b2
Robin Reyes <rmcreyes.eng@gmail.com>
2020-05-09 03:08:13 -0700

    Set up infrastructure for creating unit tests

---
+++ tests/filler_test.ts
@@ -0,0 +1,8 @@
+import { assert } from "chai";
+import "mocha";
+
+describe("Filler unit test", () => {
+  it("should pass for now and be removed when real unit tests are created", () => {
+    assert.isTrue(true);
+  });
+});
