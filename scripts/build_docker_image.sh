788490040c3011862c8380e2bea80905c61202db
Roberto Martin Reyes <rmcreyes.eng@gmail.com>
2020-05-11 00:27:50 -0700

    Make improvements to bash scripts (#34)

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
-# Delete any stale images and build a new one
-docker image rm keener-img
-docker build -t keener-img .
+set -uo pipefail
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+pushd "${ROOT_DIR}"
+
+DEV_IMG_NAME="keener-dev-img"
+
+# Delete any stale images
+printf "\nChecking for any stale dev images...\n\n"
+docker images | grep ${DEV_IMG_NAME}
+STALE_IMAGE_PRESENT=$?
+if [ $STALE_IMAGE_PRESENT -eq 0 ]; then
+    printf "\nStale dev image exists. Deleting stale image...\n\n"
+    docker image rm ${DEV_IMG_NAME}
+    STALE_IMAGE_REMOVED=$?
+
+    if [ $STALE_IMAGE_REMOVED -ne 0 ]; then
+        printf "\nFailed to delete stale dev image.\n"
+        exit 1
+    else
+        printf "\nSuccessfully deleted stale dev image.\n"
+    fi
+fi
+
+printf "\nBuilding dev image...\n\n"
+docker build -t ${DEV_IMG_NAME} .
+
+popd
+
+printf "\nDev image build successful!\n"
--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+set -eu
+
+printf "\nStarting build...\n\n"
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+pushd "${ROOT_DIR}"
+
 # TODO: Add docker image build check here
 npm run build
 npm run lint
 npm run test
+
+popd
+
+printf "\nBuild successful!\n"
