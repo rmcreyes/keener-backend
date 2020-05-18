f7f2716c84e8f6ac0e64e76623672255cb5e4208
Robin Reyes <rmcreyes.eng@gmail.com>
2020-05-18 00:59:50 -0700

    Change existing unit tests to use jest

--- src/models/test/group_test.ts
+++ src/models/test/group_test.ts
@@ -1,18 +1,14 @@
-import { expect } from "chai";
-import "mocha";
 import StudyGroup from "./../group";
 
 /**
  * Unit test suite for the study group model.
  */
-describe("Study group model", () => {
-  it("should be able to get its own ID", () => {
-    const group: StudyGroup = new StudyGroup(0, "groupName");
-    expect(group.id).to.equal(0);
-  });
+it("should be able to get its own ID", () => {
+  const group: StudyGroup = new StudyGroup(0, "groupName");
+  expect(group.id).toEqual(0);
+});
 
-  it("should be able to get its own group name", () => {
-    const group: StudyGroup = new StudyGroup(0, "groupName");
-    expect(group.groupName).to.equal("groupName");
-  });
+it("should be able to get its own group name", () => {
+  const group: StudyGroup = new StudyGroup(0, "groupName");
+  expect(group.groupName).toEqual("groupName");
 });
